da5df09dcc19582c6b52103f30efca7597f72fff
lrath99 <44142076+lrath99@users.noreply.github.com>
2018-10-26 17:43:40 -0400

    Add files via upload

--- GraphLibrary.java
+++ GraphLibrary.java
@@ -1,143 +1,98 @@
-import java.util.*;
-
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 /**
-@author Donia Tung, CS10, Dartmouth Fall 2018
-@author Lucas Rathgeb, CS10, Dartmouth Fall 2018
-
-Class containing a library of methods to be used on graphs
+ * Class containing a library of methods to be used on graphs
+ * @author Donia Tung, CS10, Dartmouth Fall 2018
+ * @author Lucas Rathgeb, CS10, Dartmouth Fall 2018
 */
 
 public class GraphLibrary{
 
   /**
-  */
-  public static <V,E> Graph<V,E> bfs(Graph<V,E> g, V source){
-    SLLQueue<V> queue = new SLLQueue<V>();
-    Graph<V,E> pathTree = new AdjacencyMapGraph<V,E>();
-    SLLQueue<V> visited = new SLLQueue<V>();
-    pathTree.insertVertex(source);
-    queue.enqueue(source);
-    visited.enqueue(source);
-    while (!queue.isEmpty()){
-      V u = queue.remove();
-      for (V v : g.outNeighbors(u)){
-        if (!visited.contains(v)){
-          visited.enqueue(v);
-          queue.enqueue(v);
-          pathTree.insertVertex(v);
-          pathTree.insertDirected(v,u,g.getLabel(u,v));
-        }
-      }
-    }
-    return pathTree;
-  }
+   * Creates a graph of all points connected to input source.
+   * @param Graph<V,E> g Input graph
+   * @param V source Center of the graph universe
+   * @throws Exception 
+   */
+ 	public static <V,E> Graph<V,E> bfs(Graph<V,E> g, V source) throws Exception{
+ 		SimpleQueue<V> holder = new SLLQueue<V>();
+ 		Set<V> visited = new HashSet<V>();
+ 		Graph<V,E> pathTree = new AdjacencyMapGraph<V,E>();
+ 		
+ 		holder.enqueue(source);
+ 		pathTree.insertVertex(source);
+ 		
+ 		while (!holder.isEmpty()){
+ 			V u = holder.dequeue();
+ 			for (V v : g.outNeighbors(u)){
+ 				if (!visited.contains(v)){
+ 					visited.add(v);
+ 					holder.enqueue(v);
+ 					pathTree.insertVertex(v);
+ 					pathTree.insertDirected(v, u, null);
+ 				}
+ 			}
+ 		}
+ 		return pathTree;
+   }
+ 	
+ 	 /**
+ 	  */
+ 	  public static <V,E> List<V> getPath(Graph<V,E> tree, V v){
+ 	    List<V> path = new ArrayList<V>();
+ 	    path.add(v);
+ 	    getPathHelpter(tree, v, path);
+ 	    return path;
+ 	  }
 
-  /**
-  */
-  public static <V,E> List<V> getPath(Graph<V,E> tree, V v){
-    List<V> path = new ArrayList<V>();
-    path.add(v);
-    getPathHelper(tree, v, path);
-    return path;
-  }
+ 	  /**
+ 	  Helper method for function getPath(). Uses recursion to construct the path that
+ 	  will lead from a given node to the center of the BFS tree.
+ 	  */
+ 	  public static <V,E> List<V> getPathHelpter(Graph<V,E> tree, V v, List<V> path){
+ 	    if (tree.outDegree(v) == 0){
+ 	      return path;
+ 	    }
+ 	    else{
+ 	      for (V vertex: tree.outNeighbors(v)){//should be only one outNeighbor
+ 	        path.add(vertex);
+ 	        getPathHelpter(tree, v, path);
+ 	      }
+ 	    }
+ 	    return path;
+ 	  }
 
-  /**
-  Helper method for function getPath(). Uses recursion to construct the path that
-  will lead from a given node to the center of the BFS tree.
-  */
-  public static <V,E> List<V> getPathHelper(Graph<V,E> tree, V v, List<V> path){
-    if (tree.outNeighbors(v) == null){
-      return path;
-    }
-    else{
-      for (V vertex: tree.outNeighbors(v)){//should be only one outNeighbor
-        path.add(vertex);
-        getPathHelpter(tree, v, path);
-      }
-    }
-  }
-
-  /*This is a non-recursive way of doing it, but I'm just confused
-because I don't know how to access the element inside of the
-outNeighbors iterable method. I also technically don't know if
-outNeighbors has a method called isEmpty() l o l
-
-  public static <V,E> List<V> getPath(Graph<V,E> tree, V v){
-    List<V> path = new List<V>();
-    V v2 = v;
-    path.add(v);
-    while (!g.outNeighbors(v2).isEmpty()){
-      path.add(v2);
-      v2 = g.outNeighbors(v2).get
-    }
-    return path;
-  }*/
-
-  /**
-  */
-
-  public static <V,E> Set<V> missingVertices(Graph<V,E> graph, Graph<V,E> subgraph){
-    Set<V> missingV = new HashSet<V>();
-    Graph<V,E> bigGraph = graph;
-    Iterable<V> bigGVertices = graph.vertices();
-    for (V vertex: subgraph.vertices()){
-      bigGraph.removeVertex(vertex);
-    }
-    for (V remaining: bigGVertices){
-      missingV.add(remaining);
-    }
-    return missingV;
-  }
-
-
-  /**
-  */
-  public static <V,E> double averageSeparation(Graph<V,E> tree, V root){
-
-
-
-  }
-
-  public static Map<Integer,String> makeMap(String pathName){
-    Map<Integer,String> idMap = new HashMap<Integer,String>;
-    try{
-      BufferedReader input = new BufferedReader(new FileReader(pathName));
-      String line;
-      while ((line = input.readLine()) != null){
-        if (line.contains["|"]){
-          String[] info = line.split("\\|");
-          int id = Integer.valueOf(info[0]);
-          if (!idMap.containsKey(id)){
-            idMap.put(id, info[1]);
-          }
-        }
-      }
-    }
-    catch (FileNotFoundException e){
-      System.out.println("File not found");
-    }
-    catch (IOException e){
-
-    }
-    finally{
-      input.close();
-    }
-    return idMap;
-  }
-
-  public static AdjacencyMapGraph<String, HashSet<String>> fileReader(HashMap<Integer,String> actorMap, HashMap<Integer,String> movieMap, String pathName){
-    AdjacencyMapGraph<String, HashSet<String>> actorMovieGraph = new AdjacencyMapGraph<String, HashSet<String>>();
-    try{
-      BufferedReader input = new BufferedReader(new FileReader(pathName));
-      String line;
-      while (line = input.readLine() != null){
-        if (line.contains["|"]){
-          info
-
-        }
-      }
-    }
-  }
-
-
+ 	  /**
+ 	  */
+ 	  public static <V,E> Set<V> missingVertices(Graph<V,E> graph, Graph<V,E> subgraph){
+ 	    HashSet<V> missingV = new HashSet<V>();
+ 	    Set<V> bigGVertices = (HashSet<V>) graph.vertices();
+ 	    for (V vertex: subgraph.vertices()){
+ 	      bigGVertices.remove(vertex);
+ 	    }
+ 	    for (V remaining: bigGVertices){
+ 	      missingV.add(remaining);
+ 	    }
+ 	    return missingV; 
+ 	  }
+ 	  
+ 	 public static <V,E> double averageSeparation(Graph<V,E> tree, V root){
+ 		 int sumDist = 0;
+ 		 sumDist = averageHelper(tree, root, sumDist);
+ 		 return ((double)sumDist)/(tree.numVertices()-1);
+ 	 }
+ 	 
+ 	 private static <V,E> int averageHelper(Graph<V,E> tree, V vertex, int sumDist) {
+ 		 if(tree.inDegree(vertex) == 0) {
+ 			 return sumDist;
+ 		 }else{
+ 			 for(V v : tree.inNeighbors(vertex)) {
+ 				sumDist = averageHelper(tree, v, sumDist + 1) + sumDist;
+ 			 }
+ 		 }
+ 		 return sumDist;
+ 	 }
 }
+
